3060f69d1e652480dd9e361d8c36596caeffd195
Helen Zhou <hz2400@nyu.edu>
2024-11-22 03:23:26 +0000

    add create wishlist at rest_api.js

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -274,5 +274,38 @@ $(function () {
     //     });
 
     // });
+    // ****************************************
+    // Create a Wishlist
+    // ****************************************
+    $("#create-btn").click(function () {
+        let name = $("#wishlist_name").val();
+        let userid = $("#wishlist_userid").val();
+        let date_create = $("#wishlist_date").val()
+
+        let data = {
+            "name": name,
+            "userid": userid,
+            "date_created": date_create,
+            "items": []
+        };
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "POST",
+            url: "/wishlists",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+        })
+        
+        ajax.done(function(res) {
+            update_wishlist_form(res);
+            flash_message("Wishlist has been Created!");
+        })
+        
+        ajax.fail(function(res) {
+            flash_message(res.responseJSON.message);
+        });
+    });
 
 })
