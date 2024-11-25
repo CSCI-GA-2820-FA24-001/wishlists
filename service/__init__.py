f000cf2920eaeeea8f44d2b779717ed4ab559588
Winston Zhang <midas0231@gmail.com>
2024-11-25 02:15:58 -0500

    fix cyclic import

--- service/__init__.py
+++ service/__init__.py
@@ -57,17 +57,17 @@ def create_app():
 
     # Initialize Plugins
     # pylint: disable=import-outside-toplevel
-    from service.models import db
-    db.init_app(app)
+    # from service.models import db
+    # db.init_app(app)
 
     with app.app_context():
         # Dependencies require we import the routes AFTER the Flask app is created
         # pylint: disable=wrong-import-position, wrong-import-order, unused-import
         from service import routes, models  # noqa: F401 E402
         from service.common import error_handlers, cli_commands  # noqa: F401, E402
-
+        models.db.init_app(app)
         try:
-            db.create_all()
+            models.db.create_all()
         except Exception as error:  # pylint: disable=broad-except
             app.logger.critical("%s: Cannot continue", error)
             # gunicorn requires exit code 4 to stop spawning workers when they die
