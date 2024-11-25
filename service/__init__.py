67948f6dd057aef69054104535a7df7895bbd9c1
Winston Zhang <midas0231@gmail.com>
2024-11-24 22:37:15 -0500

    revert db initialization

--- service/__init__.py
+++ service/__init__.py
@@ -55,16 +55,16 @@ def create_app():
         prefix="/api",
     )
 
+    # Initialize Plugins
+    # pylint: disable=import-outside-toplevel
+    from service.models import db
+    db.init_app(app)
+    
     with app.app_context():
         # Dependencies require we import the routes AFTER the Flask app is created
         # pylint: disable=wrong-import-position, wrong-import-order, unused-import
         from service import routes, models  # noqa: F401 E402
         from service.common import error_handlers, cli_commands  # noqa: F401, E402
-
-        # Initialize Plugins
-        # pylint: disable=import-outside-toplevel
-        from service.models import db
-        db.init_app(app)
 
         try:
             db.create_all()
